681ea205bd867ad8eed3baf0b7d8dd4e46cd29fd
Bartomeu Miro Mateu <bmiro@x40.lan>
2011-05-26 12:34:25 +0200

    Intentant el més bàsic

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@ all:
 	g++ -c rainer.cpp -I/usr/local/Aria/include
 	g++ rainer.o -o rainer -L/usr/local/Aria/lib/ -lrt -Llib -lAria -lpthread -ldl -Xlinker -Bstatic -lstdc++ -Xlinker -Bdynamic
 
-1:
-	g++ -c rainer1.cpp -I/usr/local/Aria/include
-	g++ rainer1.o -o rainer1 -L/usr/local/Aria/lib/ -lrt -Llib -lAria -lpthread -ldl -Xlinker -Bstatic -lstdc++ -Xlinker -Bdynamic
+wander:
+	g++ -c wander.cpp -I/usr/local/Aria/include
+	g++ wander.o -o wander -L/usr/local/Aria/lib/ -lrt -Llib -lAria -lpthread -ldl -Xlinker -Bstatic -lstdc++ -Xlinker -Bdynamic
 2:
 	g++ -c rainer2.cpp -I/usr/local/Aria/include
 	g++ rainer2.o -o rainer2 -L/usr/local/Aria/lib/ -lrt -Llib -lAria -lpthread -ldl -Xlinker -Bstatic -lstdc++ -Xlinker -Bdynamic
@@ -13,7 +13,10 @@ all:
 	g++ rainer3.o -o rainer3 -L/usr/local/Aria/lib/ -lrt -Llib -lAria -lpthread -ldl -Xlinker -Bstatic -lstdc++ -Xlinker -Bdynamic
 
 sim:
-	MobileSim -m /usr/local/Aria/maps/triangle.map &
+	#xterm -hold -e "MobileSim -m /usr/local/Aria/maps/triangle.map" &
+	xterm -hold -e "MobileSim --nomap" &
+	sleep 1
+	xterm -hold -e "./rainer"
 
 kill:
 	killall -9 MobileSim
--- rainer.cpp
+++ rainer.cpp
@@ -1,45 +1,72 @@
-/*
-   Programa d'exemple de connexió amb el robot.
-   A més a més, s'ha afegit una tasca periodica. Veure updatingTask.cpp
-*/
+/* Seguiment de trajectories, exercici 1 de robÃ²tica mÃ²bil */
 
 #include "Aria.h"
+#include <stdio.h>
+#include <float.h>
+
+struct Punt2D {
+  float x;
+  float y;
+};
+
+void anarPunt(ArRobot *rbt, Punt2D pnt) {
+  double alpha;
+  double dx, dy, d;
+  
+  d = DBL_MAX;
+  while (d >= 5.0) {
+    d = ArMath::distanceBetween(rbt->getX(), rbt->getY(), pnt.x, pnt.y);
+    
+    printf("%f\n", d);
+    
+    dx = pnt.x - rbt->getX();
+    dy = pnt.y - rbt->getY();
+    
+    alpha = ArMath::atan2(dy, dx);
+    
+    printf("X: %f, Y: %f\n", rbt->getX(), rbt->getY());
+    
+    rbt->setHeading(alpha);
+    while (!rbt->isHeadingDone(90)) {
+      rbt->setVel(0);
+    }   
+
+    rbt->setVel(150);
+    ArUtil::sleep(500);
+  }
+}
 
 int main(int argc, char **argv) {
-
   ArRobot robot3;
-
   //Declarar la tasca, dins dels constructor ja s'afegeig la tasca
+  
+  Punt2D punts[] = {{0.0, 0.0}, {0.0, 100.0}, {100.0, 0.0}, {100.0, 100.0}};
 
   Aria::init();
- 
-  
   ArSimpleConnector connector(&argc, argv);
-
-  //Testejar els arguments del programa
   if (!connector.parseArgs() || argc > 1) {
     connector.logOptions();
     exit(1);
   }
-
   if (!connector.connectRobot(&robot3)) {
     printf("Could not connect to robot... exiting\n");
     Aria::shutdown();
     return 1;
   }
-
   robot3.comInt(ArCommands::SOUNDTOG, 1);
   robot3.comInt(ArCommands::ENABLE, 1); //Habilitar els motors
-  
   robot3.runAsync(false);
-  
   while (robot3.isRunning()) {
     //Aqui va el vostre programa
-    robot3.move(1000.0);
+    for (int i = 0; i < 4 /*punts.Length*/; i++) {
+      printf("Vaig al punt %d\n", i);
+      printf("X: %f, Y: %f\n\n", robot3.getX(), robot3.getY());
+      anarPunt(&robot3, punts[i]);
+    }
   }
-
   robot3.stopRunning();
   Aria::shutdown();
   return 0;
 }
+
 
